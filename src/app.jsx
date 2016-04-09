d9af451ec718d5124d29de306e931f16aac44d2f
monotkate <Mo@MIRANDAs-MacBook-Pro.local>
2016-04-09 11:51:07 -0700

    Reverse, Display works

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import {Button, ButtonInput, Input, Form} from "react-bootstrap";
 
 var App = React.createClass({
-    render: function() {
+
+    handleChange(event) {
+        alert('change');
+        this.setState( this.validationState(event.target.value) );
+        this.setState({elem: event.target.value});
+    },
+
+    render() {
         return (
             <div>
                 <Queue />
+                <div onChange={this.handleChange}> {queueObj} </div>
             </div>
         );
     }
 });
 
 var queueObj;
+var lastObj = '';
 
 var Queue = React.createClass({
 
@@ -25,25 +34,33 @@ var Queue = React.createClass({
         }
         if (typeof elem.elem != 'undefined') {
             queueObj.push(elem.elem);
-            alert('enqueue  ' + queueObj);
+            this.handleChange(queueObj);
         }
         else {
             alert('Element does not exist');
         }
     },
 
+    //dequeues first element, shifting the rest of the array
     dequeue(event) {
         event.preventDefault();
-        if (queueObj.length > 0) {
-            alert('dequeue  ' + queueObj);
-            return queueObj.shift();
+        var lastObj;
+        let length = queueObj.length;
+        if (length > 0) {
+            lastObj = queueObj[0];
+
+            for(var i = 0; i < length-1; i++) {
+                queueObj[i] = queueObj[i+1];
+            };
+            queueObj[length-1] = null;
+            this.handleChange(queueObj);
         }
         else {
             alert('no elements to dequeue');
         }
     },
 
-    get(elem) {
+    peek(elem) {
         if (typeof queueObj[elem.elem] != 'undefined') {
             alert('give:  ' + queueObj[elem.elem]);
             return queueObj[elem.elem];
@@ -53,11 +70,38 @@ var Queue = React.createClass({
         }
     },
 
+    // On the event of a button push, reverse the array element
     reverse(event) {
         event.preventDefault();
-        queueObj.reverse();
-        alert('reversed queue:  ' + queueObj);
-        return queueObj;
+        var temp;
+        let length = queueObj.length;
+        var limit;
+
+        //Only reverse if long enough
+        if (length > 1) {
+            //If Even, flip pairs; If Odd, flip pairs around center
+            if (length % 2 === 0) {
+                limit = length/2;
+            }
+            else {
+                limit = length/2 - 1;
+            }
+
+            //Iterate halfway through and swap
+            for (var i = 0; i < limit; i++) {
+                temp = queueObj[i];
+                queueObj[i] = queueObj[length-1-i];
+                queueObj[length-1-i] = temp;
+            };
+        }
+
+        //Update display
+        this.handleChange(queueObj);
+    },
+
+    //Update the value of QueueObj for the display
+    handleChange(queue) {
+        this.setState({queueObj: queue});
     },
 
     render() {
@@ -68,15 +112,20 @@ var Queue = React.createClass({
             <div>
                 <AddElem onButtonClicked={this.enqueue} buttonValue="Enqueue"/>
                 <DisplayQueue getQueue={queueObj}/>
-                <ButtonInput type="button"
-                             value="Dequeue"
-                             bsSize="small"
-                             onClick={this.dequeue} />
-                <ButtonInput type="button"
-                             value="Reverse"
-                             bsSize="small"
-                             onClick={this.reverse} />
-                <AddElem onButtonClicked={this.get} buttonValue="Get Element"/>
+                <ButtonInput
+                    type="button"
+                    value="Dequeue"
+                    bsSize="small"
+                    onClick={this.dequeue} />
+                <div>{lastObj}</div>
+                <ButtonInput
+                    type="button"
+                    value="Reverse"
+                    bsSize="small"
+                    onClick={this.reverse} />
+                <AddElem
+                    onButtonClicked={this.peek}
+                    buttonValue="Get Element"/>
             </div>
         );
     }
@@ -117,9 +166,14 @@ var AddElem = React.createClass({
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <Input type="Add New Element" ref="input" value={this.state.elem} onChange={this.handleChange} />
-                <ButtonInput type="submit" value={this.props.buttonValue} bsStyle={this.state.style} bsSize="small" disabled={this.state.disabled}/>
-                <div> Value: {this.state.elem} </div>
+                <Input type="Add New Element"
+                       ref="input"
+                       value={this.state.elem}
+                       onChange={this.handleChange} />
+                <ButtonInput type="submit"
+                             value={this.props.buttonValue} bsStyle={this.state.style}
+                             bsSize="small"
+                             disabled={this.state.disabled}/>
             </form>
         );
     }
@@ -130,11 +184,13 @@ var DisplayQueue = React.createClass({
     render() {
         var queueList = this.props.getQueue.map(function(elem){
                         return (
-                            <Button value={elem} />
+                            <span>
+                                <ButtonInput value={elem} />
+                            </span>
                         );
                     });
         return (
-            <div> {queueList} </div>
+            <span> {queueList} </span>
         );
     }
 })
